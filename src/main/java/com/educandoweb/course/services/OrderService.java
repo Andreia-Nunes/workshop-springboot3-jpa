da44e6d4f0d41482199390c268e4e40aecf36625
Andréia Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-05-07 16:49:05 -0300

    Enhancing the application: Adding insert, delete and update in Product and Order Resources and Services

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -2,8 +2,13 @@ package com.educandoweb.course.services;
 
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +26,41 @@ public class OrderService {
 
     public Order findById(Long id){
         Optional<Order> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    public Order insert(Order obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        }
+        catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Order update(Long id, Order obj){
+        try {
+            Order entity = repository.getReferenceById(id);
+            this.updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Order entity, Order obj) {
+        entity.setOrderStatus(obj.getOrderStatus());
+
+        for(OrderItem i : obj.getItems()){
+            entity.getItems().add(i);
+        }
+
+        entity.setPayment(obj.getPayment());
+    }
 
 }
--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -3,7 +3,11 @@ package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.repositories.ProductRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +25,37 @@ public class ProductService {
 
     public Product findById(Long id){
         Optional<Product> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    public Product insert(Product obj){
+        return repository.save(obj);
+    }
 
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        }
+        catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product obj){
+        try{
+            Product entity = repository.getReferenceById(id);
+            this.updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Product entity, Product obj){
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setPrice(obj.getPrice());
+        entity.setImgUrl(obj.getImgUrl());
+    }
 }
